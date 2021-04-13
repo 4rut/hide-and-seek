8cf22f23f236f3626c5182a028186b19ed27690d
4rut <armenmen28@gmail.com>
2021-04-13 21:11:15 +0300

    code refactor

--- Python/main.py
+++ Python/main.py
@@ -1,9 +1,6 @@
 import pygame
 import os
-from random import random
-
-from random import shuffle, randrange
-
+import random
 
 this_path = os.getcwd()
 
@@ -46,7 +43,7 @@ class Player:
 
 window = Window(800, 600)
 
-
+# анимации ходьбы
 walkRight = [pygame.image.load(this_path + r'\animations\player\pygame_right_1.png'),
              pygame.image.load(this_path + r'\animations\player\pygame_right_3.png'),
              pygame.image.load(this_path + r'\animations\player\pygame_right_4.png'),
@@ -63,17 +60,45 @@ walkLeft = [pygame.image.load(this_path + r'\animations\player\pygame_left_1.png
 
 playerStand = pygame.image.load(this_path + r'\animations\player\pygame_idle.png')
 
+# создание игрока
+trump = Player(speed=5)
 
-trump = Player()
+# создание лабиринта
+grid = [[1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+        [1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+        [1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+        [1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+        [1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+        [1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+        [1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0]]
 
 
+# прорисовка окна
 def draw_window():
     global window
+
+    # заливка фона
     window.win.fill((255, 153, 51))
 
+    # прорисока лабиринта по сетке grid
+    for i in range(20):
+        for j in range(15):
+            if not grid[j][i]:
+                pygame.draw.rect(window.win, (255, 255, 255), (i * 40, j * 40, 40, 40))
+
+    # проверка синхронизации анимаций с кадрами
     if window.animCount + 1 >= 30:
         window.animCount = 0
 
+    # поворот модельки игрока
     if trump.pos_left:
         window.win.blit(walkLeft[window.animCount // 5], (trump.x, trump.y))
         window.animCount += 1
@@ -83,17 +108,24 @@ def draw_window():
     else:
         window.win.blit(playerStand, (trump.x, trump.y))
 
+    print(window.win.get_at((trump.x, trump.y)))
+
+    # смена кадра
     pygame.display.update()
 
 
+# работа окна
 run = True
 while run:
+    # настройка тикрейта
     pygame.time.Clock().tick(30)
 
+    # проверка выхода
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    # проверка передвижения игрока
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_LEFT] and trump.x > trump.speed:
@@ -102,7 +134,6 @@ while run:
         trump.pos_right = False
     elif keys[pygame.K_RIGHT] and trump.x < window.win_width - trump.speed - trump.w:
         trump.x += trump.speed
-
         trump.pos_left = False
         trump.pos_right = True
     else:
@@ -113,8 +144,13 @@ while run:
     if keys[pygame.K_UP] and trump.y > trump.speed:
         trump.y -= trump.speed
 
+    #print('1. x: ', trump.x, '\ty:', trump.y)
+
     if keys[pygame.K_DOWN] and trump.y < window.win_height - trump.speed - trump.h:
-        trump.y += trump.speed
+        if window.win.get_at((trump.x + (trump.w // 2), trump.y - trump.speed + trump.h)) != pygame.Color(255, 153, 51):
+            trump.y += trump.speed
+
+    #print('2. x: ', trump.x, '\ty:', trump.y)
 
     draw_window()
 
